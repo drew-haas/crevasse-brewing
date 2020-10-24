f1609ebdb85bf642686ed528fa1bc4be35508f24
Andrew Haas <ahaas3@apple.com>
2020-10-23 21:11:15 -0700

    add logo and h2 with styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
+    <img src="../assets/img/crevasse.jpg">
     <HelloWorld msg="Welcome to Crevasse Brewing"/>
+    <h2 class="headline">we da best</h2>
   </div>
 </template>
 
@@ -15,3 +17,11 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.headline {
+  font-size: 100px;
+  letter-spacing: 4px;
+  margin: 0;
+}
+</style>
